19e68fff0ae6e91fe73b43b6a1c78f1b7802d92c
Rodrigo Brito do Nascimento <rodrito90@gmail.com>
2019-11-20 15:39:54 -0300

    Iniciando Lab 09 Adapter

---
+++ src/testes/TestaAdapter.java
@@ -0,0 +1,15 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import exceptions.NumeroNegativoException;
+
+public class TestaAdapter {
+	
+	@Test
+	public void visitaCirvulo() throws NumeroNegativoException {
+		
+	}
+}
