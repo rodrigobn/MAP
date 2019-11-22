59960a33bb36be2138dd3764658d8d6107a6fb29
Italo Caio <it-caio@hotmail.com>
2019-11-22 00:26:28 -0300

    Colocando o Cliente

--- src/Main/Cliente.java
+++ src/Main/Cliente.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import adapter.ListMap;
 import adapter.MapList;
 
 public class Cliente {
@@ -19,12 +20,15 @@ public class Cliente {
 		String nome2 = "Aline";
 		String nome3 = "Pedro";
 		
+		System.out.println("###### MapList ######");
+		System.out.println();
 		MapList mapList = new MapList(map);
 		
 		mapList.add(nome);
 		mapList.add(nome2);
 		mapList.add(nome3);		
-		
+		System.out.println("Map de nomes:");
+		System.out.println();
 		for (int i = 0; i < mapList.toArray().length; i++) {
 			System.out.println(mapList.toArray()[i]);
 		}
@@ -41,10 +45,40 @@ public class Cliente {
 		System.out.println("Primeiro elemento = " + mapList.get(0));
 		
 		System.out.println();
+		System.out.println("tamanho? " + mapList.size());
+		
+		System.out.println();
 		mapList.clear();
 		System.out.println("Vazio? " + mapList.isEmpty());
 		
+		ListMap listMap = new ListMap(list);
 		
+		System.out.println();
+		System.out.println("###### MapList ######");
+		System.out.println();
 		
+		listMap.put(0, nome);
+		listMap.put(1, nome2);
+		listMap.put(2, nome3);
+		
+		System.out.println("Primeiro Nome da lista: " + listMap.get(0));
+		System.out.println();
+		
+		System.out.println("Tamanho: " + listMap.size());
+		System.out.println();
+		
+		System.out.println("Instancia: " + (listMap instanceof Map));
+		System.out.println();
+		
+		listMap.remove(nome);
+		
+		System.out.println("Tamanho apos remocao: " + listMap.size());
+		System.out.println();
+		
+		System.out.println("Vazia? " + listMap.isEmpty());
+		System.out.println();
+		listMap.clear();
+		System.out.println("Vazia apos o clean? " + listMap.isEmpty());
+		System.out.println();
 	}
 }
